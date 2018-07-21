367fa3413e669179bb64b31e458a3a5df78e040d
yoyoyousei <yoyoyousei2016@gmail.com>
2018-07-21 19:13:52 +0900

    update zsh, tmux settings

--- .zshrc
+++ .zshrc
@@ -4,14 +4,18 @@ function precmd() {
   fi
 }
 
+# zsh-completion
+fpath=(/usr/local/share/zsh-completions $fpath)
+autoload -Uz compinit && compinit -u
+
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then source '~/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '$HOME/google-cloud-sdk/path.zsh.inc' ]; then source '$HOME/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then source '~/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '$HOME/google-cloud-sdk/completion.zsh.inc' ]; then source '$HOME/google-cloud-sdk/completion.zsh.inc'; fi
 
-if [ -f ~/aliases.zsh ] && source ~/aliases.zsh
-if [ -f ~/.zshrc.local ] && source ~/.zshrc.local
+if [ -f ~/aliases.zsh ]; then source ~/aliases.zsh; fi
+if [ -f ~/.zshrc.local ]; then source ~/.zshrc.local; fi
 
 # setup fasd
 eval "$(fasd --init auto)"
@@ -28,9 +32,11 @@ function peco-src(){
 }
 zle -N peco-src
 
-export NVM_DIR=“$HOME/.nvm”
-[ -s “$NVM_DIR/nvm.sh” ] && \. “$NVM_DIR/nvm.sh”  # This loads nvm
-# [ -s “$NVM_DIR/bash_completion” ] && \. “$NVM_DIR/bash_completion”  # This loads nvm bash_completion
+# fix home, end, delete keys
+# To investigate the code of key, od -c, press any key, enter, then C-c.
+bindkey "\033[1~" beginning-of-line # Home
+bindkey "\033[4~" end-of-line       # End
+bindkey "^[[3~"  delete-char        # Delete
 
 LANG="`defaults read -g AppleLocale | sed 's/@.*$//g'`.UTF-8"
 export LANG
@@ -39,3 +45,9 @@ export LANG
 source <(stern --completion=zsh)
 # set env_val for gcp pubsub emulator
 # $(gcloud beta emulators pubsub env-init)
+
+
+# nvm setup
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- aliases.zsh
+++ aliases.zsh
@@ -7,42 +7,42 @@ alias sd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
-alias v=‘f -e vim’ # quick opening files with vim
-alias m=‘f -e mplayer’ # quick opening files with mplayer
-alias o=‘a -e xdg-open’ # quick opening files with xdg-open
+alias v='f -e vim' # quick opening files with vim
+alias m='f -e mplayer' # quick opening files with mplayer
+alias o='a -e xdg-open' # quick opening files with xdg-open
 
 # etc
 alias ls='ls -al'
-alias ggt=‘go get -v’
-alias psthis=‘git push -u origin HEAD’
-alias xno=‘xargs -n1 open’
+alias ggt='go get -v'
+alias psthis='git push -u origin HEAD'
+alias xno='xargs -n1 open'
 
 # gcloud, kube
-alias gchange=‘gcloud config configurations activate’
-alias gcl=‘gcloud’
+alias gchange='gcloud config configurations activate'
+alias gcl='gcloud'
 
-alias kube=‘kubectl’
-alias kp=‘kube-prompt’
+alias kube='kubectl'
+alias kp='kube-prompt'
 
-alias fblogin=‘firebase login’
-alias fblogout=‘firebase logout’
-alias fbdeploy=‘firebase deploy’
-alias fbuse=‘firebase use’
-alias fbconfig=‘firebase functions:config:get’
+alias fblogin='firebase login'
+alias fblogout='firebase logout'
+alias fbdeploy='firebase deploy'
+alias fbuse='firebase use'
+alias fbconfig='firebase functions:config:get'
 
-alias gn=‘git now .’
+alias gn='git now .'
 
 alias git=hub
-alias gpr=‘git pull-request’
-alias gfp=‘git fetch --prune’
+alias gpr='git pull-request'
+alias gfp='git fetch --prune'
 
 # Global aliases
-alias -g ...=‘../..’
-alias -g ....=‘../../..’
-alias -g .....=‘../../../..’
-alias -g C=‘| wc -l’
-alias -g H=‘| head’
-alias -g L=“| less”
-alias -g N=“| /dev/null”
-alias -g S=‘| sort’
-alias -g G=‘| grep’ # now you can do: ls foo G something
+#alias -g ...='../..'
+#alias -g ....='../../..'
+#alias -g .....='../../../..'
+alias -g C='| wc -l'
+alias -g H='| head'
+alias -g L='| less'
+alias -g N='| /dev/null'
+alias -g S='| sort'
+alias -g G='| grep' # now you can do: ls foo G something

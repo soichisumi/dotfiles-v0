6f57994562bfb4142d7972eb566ae52c3b96898a
yoyoyousei <atom.soichi0407@gmail.com>
2018-07-31 10:42:07 +0900

    exaを追加し、.gitconfigで.gitmessageを読み込むようにした

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ brew update
 brew tap c-bata/kube-prompt
 brew install autoconf automake bash-completion boost carthage cmake coreutils ctags czmq fasd fontconfig fontforge ghq git git-now go gradle htop hub ios-deploy ios-webkit-debug-proxy jq kube-prompt kubernetes-cli kubernetes-helm libbitcoin libbitcoin-explorer libbitcoin-network maven mongodb mysql nginx nsq oniguruma openssl peco perl pkg-config protobuf pyenv qt rbenv rbenv-gemset readline ricty ruby ruby-build sqlite stern stunnel swiftformat swiftlint tig tmux trash tree usbmuxd vim watch wget yarn zeromq zsh zsh-completions
 brew install zsh-autosuggestions
+brew install exa
